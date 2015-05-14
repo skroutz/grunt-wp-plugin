8a32c78aa7c29ee852f845097637266329536093
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-05-14 23:52:57 +0545

    Initial workflow

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+/*
+ * grunt-wp-svn
+ * https://github.com/axisthemes/grunt-wp-svn
+ *
+ * Copyright (c) 2014 AxisThemes
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+	// Project Configuration.
+	grunt.initConfig({
+
+		// Validate files with JSHint.
+		jshint: {
+			all: [
+				'Gruntfile.js',
+				'tasks/*.js',
+				'<%= nodeunit.tests %>'
+			],
+			options: {
+				jshintrc: '.jshintrc'
+			}
+		},
+
+		// Before generating any new files, remove any previously-created files.
+		clean: {
+			tests: ['tmp']
+		},
+
+		// Configuration to be run (and then tested).
+		wpsvn: {},
+
+		// Unit tests.
+		nodeunit: {
+			tests: ['test/*_test.js']
+		}
+	});
+
+	// Actually load this plugin's task(s).
+	grunt.loadTasks( 'tasks' );
+
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks( 'grunt-contrib-clean' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-contrib-nodeunit' );
+
+	// Whenever the "test" task is run, first clean the "tmp" dir, then run this
+	// plugin's task(s), then test the result.
+	grunt.registerTask( 'test', ['clean', 'wpsvn', 'nodeunit'] );
+
+	// Register default task.
+	grunt.registerTask( 'default', ['jshint', 'test'] );
+};
---
+++ tasks/wpsvn.js
@@ -0,0 +1,18 @@
+/*
+ * grunt-potomo
+ * https://github.com/axisthemes/grunt-wp-svn
+ *
+ * Copyright (c) 2014 AxisThemes
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var shell = require( 'shelljs' );
+
+module.exports = function( grunt ) {
+
+	grunt.registerMultiTask( 'potomo', 'Deploy project directory to WordPress SVN repo.', function() {
+
+	});
+};
