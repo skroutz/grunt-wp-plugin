3ad4d592c7894cb408148b75faf157a939e2c1cd
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-05-18 09:41:55 +0545

    Change task

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function( grunt ) {
 		},
 
 		// Configuration to be run (and then tested).
-		wpsvn: {
+		wp_plugin: {
 			options: {
 				svn_username: 'shivapoudel',
 				svn_repository: 'https://svn.riouxsvn.com/{plugin-slug}'
